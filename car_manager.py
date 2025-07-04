177009b8692d25f81fa3db01e0cbcf575fa22e9b
ab-tai-404 <tailaba999@gmail.com>
2025-07-04 19:20:01 +0100

    The first step

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,19 @@
+import random
+from turtle import Turtle
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.y = random.randint(-280,280)
+        self.turtlesize(1,2)
+        self.penup()
+        self.shape("square")
+        self.color(random.choice(COLORS))
+        self.goto(320 ,self.y)
+        self.setheading(180)
+    def move(self):
+        self.fd(STARTING_MOVE_DISTANCE)
--- main.py
+++ main.py
@@ -5,10 +5,28 @@ from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
+screen.listen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
-
+player =Player()
+screen.onkey(fun=player.move, key="Up")
 game_is_on = True
+cars = []
+i = 0
 while game_is_on:
+    if i % 6 == 0   :
+        car = CarManager()
+        cars.append(car)
+    for c in cars :
+        c.move()
+        if c.xcor() < 30 and player.distance(c) < 30 :
+            game_is_on = False
+            scoreboard =Scoreboard()
     time.sleep(0.1)
     screen.update()
+
+
+
+    i += 1
+
+screen.exitonclick()
